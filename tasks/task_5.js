77135b26a7476a3caaaa130c01258a540fcbd40c
Andrii Kulyk <kyluk5@gmail.com>
2020-05-16 20:07:57 +0300

    first_done

---
+++ tasks/task_5.js
@@ -0,0 +1,29 @@
+// debugger;
+
+const checkForSpam = function (message) {
+    // твой код
+    const words = message.split(' ');
+
+    // console.log(words);
+
+    for (let result of words) {
+
+
+        if () {
+            // return true;
+        } else {
+            // return false;
+        }
+    }
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+// console.log(checkForSpam('Latest technology news')); // false
+
+// console.log(checkForSpam('JavaScript weekly newsletter')); // false
+
+console.log(checkForSpam('Get best sale offers now!')); // true
+
+// console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
